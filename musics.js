c74046b89cd6a4fee998c37a30db23eeaafe09e7
Ryenan <ryenanaragaorodrigues@gmail.com>
2024-06-21 12:15:07 -0300

    Terminei o site

--- musics.js
+++
@@ -1,6 +0,0 @@
-export default[
-    {
-        src: "/imgs/Tribo da Periferia - RESILIÊNCIA.mp3",
-        name: "Resiliência"
-    }
-]
--- script.js
+++ script.js
@@ -277,4 +277,5 @@ function obrigadoContato(){
         })
     }
     
-
+    
+    
